a72f9447286884955f34169ac5c5254a00022c51
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-01 06:29:10 +0530

    Delete data/u.js

--- data/u.js
+++
@@ -1,9 +0,0 @@
-window.data = {
-   "uranus": {
-        text: "Uranus is the seventh planet from the Sun and the third-largest in diameter in our Solar System. Known as the \"ice giant,\" it has a pale blue-green color due to methane in its atmosphere, which absorbs red light and reflects blue and green light. Uranus is unique because it rotates on its side, with an axial tilt of 98 degrees, likely caused by a massive collision early in its history. This unusual tilt results in extreme seasonal variations, with each pole experiencing 42 years of sunlight followed by 42 years of darkness. Discovered by William Herschel in 1781, Uranus has at least 27 moons, the largest of which include Titania and Oberon, and is surrounded by faint rings made of ice and dust. Its atmosphere consists mostly of hydrogen, helium, and methane, with frigid temperatures averaging around -224°C (-371°F), making it one of the coldest planets in the Solar System.",
-        Url: "https://www.ecosia.org/search?q=uranus",
-        blog: "https://bhuvaneshm-dev.blogspot.com/2025/01/uranus-mysterious-ice-giant-of-our.html",
-        imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgyngCH0lYp4Q8guVC0KeN-0pYCnA3_uwbACAm4jAi4mSg0hl6uwl1E1JW1aKj0MQAk2uTlAwqIbY-rcWE7Yt3__4RJKn7TjlIsPfJ0wFu3vuZbCADorbC57JjtPj1zKcDJRrg4wWPa__9G6YLlni4axpwFxYq9UO4yQBK-JSeBAfFRTGcWAduYmruAS6WV/s1024/uranus.jpg"
-   }
-};
-
