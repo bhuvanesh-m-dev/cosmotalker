ed42f99e2080a55b7206231a800d26d51c3d3bdc
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-01 06:28:22 +0530

    Delete data/j.js

--- data/j.js
+++
@@ -1,8 +0,0 @@
-window.data = {
-    "jupiter":{
-        text:"Jupiter is the largest planet in our solar system, a gas giant primarily composed of hydrogen and helium. It is the fifth planet from the Sun and is known for its striking features, including its iconic Great Red Spot, a massive storm that has been raging for centuries. Jupiter has a strong magnetic field and at least 92 moons, the largest of which are the Galilean moons: Io, Europa, Ganymede, and Callisto. Its rapid rotation gives it a distinctive oblate shape, and its dynamic atmosphere features colorful bands and storms. Jupiter plays a crucial role in shaping the solar system's dynamics due to its immense size and gravitational influence.",
-        blog:"https://bhuvaneshm-dev.blogspot.com/2025/01/jupiter-majestic-giant-of-our-solar.html",
-        Url:"https://www.ecosia.org/search?q=jupiter",
-        imageUrl:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiEzt2a79nvjZLJG8jbzwQb6Md8yMBoh0l-1EqvgeDiLqLIvCDFWDk52YxAWDPPnarc9OnBv9W2vOWZYeHdoPQaDgH9_2dDwI9o48pFcQuwQci2GhMc6TuFk6q7IjoWict3gbapsafOt5pYo-yW24DSCxb7YlPHIA_6Fg12lQZ96gJf8faEYcI3PBHYxdmo/s1412/jupiter.jpg"
-    }
-};
