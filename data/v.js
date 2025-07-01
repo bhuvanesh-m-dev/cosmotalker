2cd47f220d6b22b4ac6e496138359f9801faaa94
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-01 06:29:20 +0530

    Delete data/v.js

--- data/v.js
+++
@@ -1,8 +0,0 @@
-window.data = {
-    "venus": {
-        text: "Venus is the second planet from the Sun and often referred to as EARTH'S SISTER planet due to its similar size and composition. It is the hottest planet in the solar system, with surface temperatures averaging around 475°C (900°F), primarily because of its thick atmosphere composed mainly of carbon dioxide, which creates a runaway greenhouse effect. Venus has a dense layer of clouds made of sulfuric acid, making it highly reflective and bright, often visible to the naked eye as the MORNING STAR or EVENING STAR. Unlike most planets, Venus rotates in the opposite direction of its orbit (retrograde rotation) and takes about 243 Earth days to complete one rotation, making its day longer than its year. Despite its harsh conditions, Venus remains a point of interest for scientists studying planetary atmospheres and the potential for ancient water presence.",
-        url: "https://www.ecosia.org/search?q=venus",
-        blog:"https://bhuvaneshm-dev.blogspot.com/2025/01/venus-facts-you-cant-miss-good-hot-and.html",
-        imageUrl: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjfKb4qe744Mw7IYq0RAuH7qWUIECjtbfa5FGHH5FclhpV_hEmVF96p5-TbWx_0lK7Fpr9zXxL-R1xDfqlPS22L5yEcY-RVsVX_WsJNL4WNOVDI7Oc5W4IKC89btEZC1LJPgsmkfDARwIdcsAM3GdwfnhvM-frqY2zVe4TLuTHctVi-EMDJlTbg-YX1iJ5v/s800/venus.webp"
-    }
-};
