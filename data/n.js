d42aabdb12b38f1f31011b5f1e92ccca0931806d
BHUVANESH M <bhuvaneshm.developer@gmail.com>
2025-07-01 06:28:42 +0530

    Delete data/n.js

--- data/n.js
+++
@@ -1,8 +0,0 @@
-window.data = {
-  "neptune":{
-        text:"Neptune is the eighth and farthest planet from the Sun in our solar system, with a diameter of about 49,244 kilometers. It is a gas giant composed primarily of hydrogen, helium, and methane, the latter of which gives the planet its characteristic blue color. Neptune has a very active atmosphere, with strong winds, storms, and the fastest winds in the solar system, reaching speeds of up to 2,100 kilometers per hour. The planet has a faint ring system and 14 known moons, with Triton being the largest and most notable. Triton is unique because it has a retrograde orbit, meaning it orbits Neptune in the opposite direction of the planet's rotation, and is believed to have been captured by Neptune’s gravity. Neptune is also known for its Great Dark Spot, a storm similar to Jupiter’s Great Red Spot, though it has since disappeared. Despite its distance from the Sun, Neptune's intense internal heat drives much of its weather systems.",
-        Url:"https://www.ecosia.org/search?q=neptune",
-        blog:"https://bhuvaneshm-dev.blogspot.com/2025/01/neptune-mysterious-blue-giant-at-edge.html",
-        imageUrl:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi4-50kG1YJUTr2eLEv7nWT-L-Q5VfutU8c-erc-w0M0sg5GWmOrfWFYyq51-fBKnHZ6tbSTWj1y75sAN230Wlq3jesZme2ao-tewUNNJZOiDOI1mG7I2UNZGYJ6rYjw_1MvAUEHYENXqMzmok2NHY4ufaIML2jpcOVdK2JpdwkzF6v6h6hSNVUsGvJmz4c/s640/neptune.jpg"
-    }
-};
